bfa318757d35a19304efa8ad6576898dabaeed8f
Rachit <rachitbhatt2000@gmail.com>
2024-12-05 14:20:09 -0500

    Rewriting code.

--- city-pulse/src/app/app.component.spec.ts
+++ city-pulse/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'GitHub-Deployment' title`, () => {
+  it(`should have the 'city-pulse' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GitHub-Deployment');
+    expect(app.title).toEqual('city-pulse');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, GitHub-Deployment');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, city-pulse');
   });
 });
